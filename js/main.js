fe278603161497c3a1b9d8569bb589fe3d98e91d
David Gilbert <d_gilbert5@yahoo.com>
2018-08-05 15:34:43 -0400

    Initial commit of AngularJS Chuck Norris Joke Generator

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var app = angular.module('myApp', []);
+
+app.controller('myCtrl', ['$scope', '$http', function($scope, $http) {
+  $scope.jokes = [];
+  var obj = {};
+
+  // get a random joke so a joke is able to be displayed initially. Exclude explict jokes
+  $.getJSON('https://api.icndb.com/jokes/random?exclude=[explicit]', function(data) {
+    obj.joke = _.unescape(data.value.joke);
+    $scope.jokes.push(obj);
+    $scope.$apply();
+  });
+
+  // on click handler that gets a random joke when is clicked
+  $('.btn-primary').on('click', function() {
+    $scope.jokes = [];
+    obj = {};
+
+    // Exclude explict jokes
+    $.getJSON('https://api.icndb.com/jokes/random?exclude=[explicit]', function(data) {
+        obj.joke = _.unescape(data.value.joke);
+        $scope.jokes.push(obj);
+        $scope.$apply();
+    });
+  });
+
+}]);
